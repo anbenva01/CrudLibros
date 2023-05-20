c54fff50bee75485eae696958298e00be3019091
ANDRES <andresb@example.com>
2023-05-20 12:06:29 -0500

    save, delete, modify and create libros formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,3 @@
-import { addDoc, collection } from 'firebase/firestore';
 import React, {useState,useEffect} from 'react'
 import {db} from '../firebase'
 import { collection, doc, addDoc, deleteDoc, updateDoc, onSnapshot } from 'firebase/firestore'
@@ -7,18 +6,25 @@ const formulario = () => {
     const [nombreLibro, setNombreLibro] = useState('');
     const [nombreAutor, setNombreAutor] = useState('');
     const [listaLibros, setListaLibros] = useState([]);
+    const [id,setId] = useState(0);
+    const [modoEdicion, setModoEdicion] = useState(false);
 
     useEffect(()=>{
+       
         const obtenerDatos = async() =>{
             try {
                 await onSnapshot(collection(db,'libros'),(query) =>{
-                    setListaLibros(query.docs.map((doc)=>({...doc.data(),id:doc.id})))
+                    console.log('entro');
+                    setListaLibros(query.docs.map((doc)=>{
+                        return {...doc.data(),id:doc.id}
+                    }))
+                    
                 })
             } catch (error) {
                 console.log(error)
             }
-            obtenerDatos();
         }
+        obtenerDatos();
     },[])
 
     const guardarLibros = async(e) =>{
@@ -40,27 +46,86 @@ const formulario = () => {
         }
     }
 
+    const eliminarDatos = async id => {
+        try {
+            await deleteDoc(doc(db,'libros',id))
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const editarDatos = item =>{
+        setNombreLibro(item.nombreLibro);
+        setNombreAutor(item.nombreAutor);
+        editable(item);
+    }
+
+    const editable = item => {
+        setNombreLibro(item.nombreLibro)
+        setNombreAutor(item.nombreAutor)
+        setId(item.id)
+        setModoEdicion(true)
+    }
+
+    const editarLibros = async(e) =>{
+        e.preventDefault();
+        try {
+            const docRef = doc(db,'libros',id);
+            await updateDoc(docRef,{
+                nombreLibro: nombreLibro,
+                nombreAutor: nombreAutor
+            })
+            const nuevoArray = listaLibros.map(
+                item => item.id === id ? {id:id, nombreLibro:nombreLibro,nombreAutor,nombreAutor}:item
+            )
+            setListaLibros(nuevoArray);
+            setNombreAutor('')
+            setNombreLibro('')
+            setId('')
+            setModoEdicion(false)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const cancelar = () =>{
+        setModoEdicion(false)
+        setNombreLibro('')
+        setNombreAutor('')
+        setId('')
+    }
+
   return (
     <div className='container mt-5'>
         <h1 className='text-center'> CRUD DE LIBROS</h1>
         <hr/>
         <div className="row">
         <div className="col-8">
-            <h4 className='text-center'>Listado de libros</h4>
-            <ul className='list-group'>
-                <li className='list-group-item'>
-                    <span className='lead'>test</span>
-                    <button className='btn btn-danger btn-sm float-end mx-2'>Eliminar</button>
-                    <button className='btn btn-warning btn-sm float-end'>Editar</button>
-                </li>
+            <h4 className="text-center">Listado de libros</h4>
+            <ul className="list-group">
+                {
+                    listaLibros.map(item =>(
+                        <li className="list-group-item" key={item.id}>
+                            <span className="lead">{item.nombreLibro} - {item.nombreAutor}</span>
+                            <button onClick={()=>eliminarDatos(item.id)} className='btn btn-danger btn-sm float-end mx-2'>Eliminar</button>
+                            <button onClick={()=>editarDatos(item)} className='btn btn-warning btn-sm float-end'>Editar</button>
+                        </li>
+                    ))
+                }
             </ul>
         </div>
         <div className='col-4'>
-            <h4 className='text-center'>agregar libros</h4>
-            <form onSubmit={guardarLibros}>
+            <h4 className='text-center'>{modoEdicion ? 'Editar libros' : 'Agrega libros'}</h4>
+            <form onSubmit={modoEdicion ? editarLibros : guardarLibros}>
                 <input type="text" className='form-control mb-2' onChange={(e)=>setNombreLibro(e.target.value)} value={nombreLibro} placeholder='Ingrese nombre libro' />
                 <input type="text" className='form-control mb-2' onChange={(e)=>setNombreAutor(e.target.value)} value={nombreAutor} placeholder='Ingrese nombre autor' />
-                <button className='btn btn-primary btn-block'>Agregar</button>
+                {
+                    modoEdicion ? (
+                        <><button className='btn btn-warning btn-block'>Editar</button>
+                        <button onClick={()=>cancelar()} className='btn btn-dark btn-block mx-2'>Cancelar</button></>
+                    ):(<><button className='btn btn-primary btn-block'>Agregar</button></>)
+                    
+                }
             </form>
         </div>
         </div>    
